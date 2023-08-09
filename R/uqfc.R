2bafc71fd7edae7555a922e9920f2c89a0c9dbf8
fredbec <f-becker@posteo.de>
2023-08-09 16:59:16 +0200

    scoring function

--- R/uqfc.R
+++ R/uqfc.R
@@ -1,4 +1,4 @@
-#' Function to download, tidy and save WEO data
+#' Function to extract historical empirical quantiles from WEO data
 #'
 #' @param weodat (subset of) WEO data as returned by download.process.weo()
 #' function in package imfpp
@@ -6,7 +6,7 @@
 #' @param tv_release which release of the true value in the WEO data shall be
 #' used. Must be one of c(0.5, 1, 1.5, 2)
 #'
-#' @return nothing, saves tidied data in directory
+#' @return data.table with empirical quantiles
 #' @export
 #'
 
@@ -17,16 +17,22 @@ empqu <- function(weodat,
 
   .d <- `[`
 
+  #####Input checks####
   if (!(tv_release %in% c(0.5, 1, 1.5, 2))){
 
     stop("tv_release must be one of c(0.5, 1, 1.5, 2)")
   }
 
-  calculate_quantiles <- function(x) {
-    quantile(x, probs = quantiles)
+  if(!(horizon %in% seq(0, 5, by = 0.5))){
+
+    stop("invalid value for horizon")
   }
 
 
+  #small helper function for calculating quantiles
+  calculate_quantiles <- function(x) {
+    quantile(x, probs = quantiles)
+  }
 
   quants <-
     weodat |>
@@ -35,11 +41,25 @@ empqu <- function(weodat,
     .d(!is.na(error)) |>
     .d(, {
       quantile_vals <- calculate_quantiles(error)
-      .(quantile = paste0("quant", quantiles),
+      .(quantile = quantiles,
         setNames(quantile_vals, paste0("quant", quantiles)))
     }, by = country) |>
     setnames("V2", "value")
 
 
   return(quants)
+}
+
+#' Function to download, tidy and save WEO data
+#'
+#' @param empquants
+#' @return nothing, saves tidied data in directory
+#' @export
+#'
+score_quants <- function(empquants,
+                         weodat,
+                         sRule = scoringRules::crps_sample){
+
+  sapply(weodat, function(dp) sRule(dp, empquants))
+
 }
