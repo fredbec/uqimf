1b37421e7205762d5d4307eeff7f095354e490a8
fredbec <f-becker@posteo.de>
2023-08-22 10:35:08 +0200

    extend target_year set, keep both prv_year and prv_period measures

--- oecd_data/oecd_gdp_clean_quarterly.R
+++ oecd_data/oecd_gdp_clean_quarterly.R
@@ -5,10 +5,10 @@ library(dplyr)
 
 oecd <- read.csv(here("oecd_data", "oecd_gdp.csv"))
 
+#read in quarterly data
 oecd_q <- oecd |>
   filter(FREQUENCY == "Q",
          SUBJECT == "TOT") |>
-  #filter(LOCATION %in% c("CAN", "FRA", "DEU", "ITA", "JPN", "GBR", "USA")) |>
   select(-c(INDICATOR, SUBJECT, FREQUENCY, Flag.Codes)) |>
   rename(country = LOCATION,
          measure = MEASURE,
@@ -16,24 +16,29 @@ oecd_q <- oecd |>
          value = Value) |>
   mutate(target_year = substr(yrq, 1, 4) |> as.numeric(),
          quarter = substr(yrq, 7, 7) |> as.numeric()) |>
-  filter(target_year >= 1980) |> #PC_CHGPP refers to previous period, PC_CHGPY refers to same period previous year
+  filter(target_year >= 1960) |>
   rename(ccode = country) |>
-  select(-yrq)
+  select(-yrq)|>
+  mutate(type = ifelse(measure == "PC_CHGPY", "prv_year", "prv_period")) |> #PC_CHGPP refers to previous period, PC_CHGPY refers to same period previous year
+  select(-measure)
 
 
+#read in yearly data
 oecd_y <- oecd |>
   filter(FREQUENCY == "A",
          SUBJECT == "TOT") |>
-  #filter(LOCATION %in% c("CAN", "FRA", "DEU", "ITA", "JPN", "GBR", "USA")) |>
   select(-c(INDICATOR, SUBJECT, FREQUENCY, Flag.Codes)) |>
   rename(country = LOCATION,
          measure = MEASURE,
          target_year = TIME,
          value = Value) |>
   mutate(quarter = "Y") |>
-  filter(target_year >= 1980) |> #PC_CHGPP refers to previous period, PC_CHGPY refers to same period previous year
-  rename(ccode = country)
+  filter(target_year >= 1960) |>
+  rename(ccode = country) |>
+  mutate(type = ifelse(measure == "PC_CHGPY", "prv_year", "prv_period")) |> #PC_CHGPP refers to previous period, PC_CHGPY refers to same period previous year
+  select(-measure)
 
+#bind the two together
 oecd <- rbind(oecd_q, oecd_y)
 
 data.table::fwrite(oecd, here("oecd_data", "oecd_gdp_quarterly_annual.csv"))
