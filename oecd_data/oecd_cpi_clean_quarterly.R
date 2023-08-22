bc9c4d5942778a5c8b2fd67f7a75780e93edadef
fredbec <f-becker@posteo.de>
2023-08-22 10:37:10 +0200

    change data source. extend target_year, keep annual+quarterly, prv_year+ prv_period

--- oecd_data/oecd_cpi_clean_quarterly.R
+++ oecd_data/oecd_cpi_clean_quarterly.R
@@ -4,38 +4,25 @@ library(data.table)
 library(dplyr)
 
 
-oecd <- read.csv(here("oecd_data", "oecd_cpi.csv"))
-
-oecd_q <- oecd |>
-  filter(FREQUENCY == "Q",
-         SUBJECT == "TOT") |>
-  #filter(LOCATION %in% c("CAN", "FRA", "DEU", "ITA", "JPN", "GBR", "USA")) |>
-  select(-c(INDICATOR, SUBJECT, FREQUENCY, Flag.Codes)) |>
-  rename(country = LOCATION,
+oecd <- read.csv(here("oecd_data", "oecd_cpi_quarter.csv")) |>
+  filter(Subject == "CPI: 01-12 - All items",
+         MEASURE %in% c("GY", "GP")) |>
+  select(-c(Measure, SUBJECT, Subject)) |>
+  select(LOCATION, Country, MEASURE, TIME, Unit, Value) |>
+  rename(ccode = LOCATION,
+         country = Country,
          measure = MEASURE,
          yrq = TIME,
          value = Value) |>
   mutate(target_year = substr(yrq, 1, 4) |> as.numeric(),
          quarter = substr(yrq, 7, 7) |> as.numeric()) |>
-  filter(target_year >= 1980) |>
+  mutate(quarter = ifelse(is.na(quarter), "Y", quarter)) |>
   select(-yrq) |>
-  rename(ccode = country)
-
-
-oecd_y <- oecd |>
-  filter(FREQUENCY == "A",
-         SUBJECT == "TOT") |>
-  #filter(LOCATION %in% c("CAN", "FRA", "DEU", "ITA", "JPN", "GBR", "USA")) |>
-  select(-c(INDICATOR, SUBJECT, FREQUENCY, Flag.Codes)) |>
-  rename(country = LOCATION,
-         measure = MEASURE,
-         value = Value,
-         target_year = TIME) |>
-  mutate(quarter = "Y") |>
-  rename(ccode = country)
-
-
-oecd <- rbind(oecd_q, oecd_y)
-
+  mutate(type = ifelse(measure == "GY", "prv_year", "prv_period")) |>
+  mutate(kick = ifelse(type == "prv_year" & quarter == "Y", 1, 0)) |> #redundant
+  filter(kick == 0) |>
+  select(-kick) |>
+  select(-c(Unit, measure)) |> #redundant
+  arrange(ccode)
 
 data.table::fwrite(oecd, here("oecd_data", "oecd_cpi_quarterly_annual.csv"))
