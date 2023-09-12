5ac97e7d6509a2247fdee9724c7ac71ba1ec5461
fredbec <f-becker@posteo.de>
2023-09-12 18:57:02 +0200

    score bvar quantile forecasts

--- scripts/score-forecasts.R
+++ scripts/score-forecasts.R
@@ -63,13 +63,15 @@ fcdat <- hordat[fcdat, on = c("target_year", "forecast_year", "season")] |>
 
 
 
-#######read in quantile forecasts
+####################################read in quantile forecasts########################
 qufcs <- data.table::fread(here("quantile_forecasts", "quantile_forecasts.csv"))
 combs <- data.table::fread(here("quantile_forecasts", "setting_combinations.csv"))
+bvar_qufcs <- data.table::fread(here("benchmarks", "forecast_quantiles.csv"))
 
 
 #################score CI's####################################################
 weodat_qu_sameyearset <- qufcs |>
+  data.table::copy() |>
   .d(target_year>=1999) |>
   .d(,.(country, target, horizon, target_year, true_value, prediction, quantile, method, error_method, source)) |>
   setnames("source", "model") |>
@@ -84,7 +86,7 @@ weodat_qu_allyears <- qufcs |>
 scores <- scoreempQu(weodat_qu_sameyearset, cvg_rg = c(50,80),
                      by = c("model", "error_method", "method", "country", "target", "horizon"))
 
-scores_allyears <- scoreempQu(weodat_qu_sameyearset, cvg_rg = c(50,80),
+scores_allyears <- scoreempQu(weodat_qu_allyears, cvg_rg = c(50,80),
                      by = c("model", "error_method", "method", "country", "target", "horizon"))
 
 scores_avgcountry <- scoreempQu(weodat_qu_sameyearset, cvg_rg = c(50,80),
@@ -148,10 +150,70 @@ all_crps <- lapply(1:nrow(combs_methods), function(idx){
   .d(, .(score = mean(score)), by = c("target", "horizon", "method", "error_method", "source"))
 
 
+
+
+####################################Score BVAR Quantile Forecasts###########################
+idx_bvar <- which(bvar_qufcs$quantile_level == 0.05)[1] #where bvar starts
+
+#remove redundant rows and rename target
+bvar_qus <- hordat[bvar_qufcs[idx_bvar:nrow(bvar_qufcs)], on = c("target_year", "forecast_year", "season")]  |>
+  .d(, target := ifelse(var == "cpi", "pcpi_pch", "ngdp_rpch")) |>
+  .d(, var := NULL)  |>
+  .d(, source := "bvar") |>
+  .d(, method := NULL) |>
+  setnames("value", "prediction") |>
+  .d(target_year >= min_year & target_year <= max_year) |>
+  setnames("quantile_level", "quantile")
+
+#join with truth data
+bvar_qus <- truth[bvar_qus, on = c("target", "target_year", "country") ]  |>
+  .d(order(source, target, country, forecast_year, horizon)) |>
+  .d(, .(source, target, country, forecast_year, horizon, target_year,
+         prediction, get(paste0("tv_", tv_release)), quantile)) |>
+  setnames("V8", paste0("tv_", tv_release)) |>
+  setnames(paste0("tv_", tv_release), "true_value") |>
+  .d(target_year <= max_year)
+
+
+bvar_qu_sameyearset <- bvar_qus |>
+  data.table::copy() |>
+  .d(target_year>=1999) |>
+  .d(,.(country, target, horizon, target_year, true_value, prediction, quantile, source)) |>
+  setnames("source", "model") |>
+  .d(quantile %in% c(0.1, 0.25, 0.75, 0.9))
+
+bvar_qu_allyears <- bvar_qus |>
+  .d(!is.na(horizon)) |>
+  .d(,.(country, target, horizon, target_year, true_value, prediction, quantile,  source)) |>
+  setnames("source", "model") |>
+  .d(quantile %in% c(0.1, 0.25, 0.75, 0.9))
+
+
+bvar_scores <- scoreempQu(bvar_qu_sameyearset, cvg_rg = c(50,80),
+                     by = c("model", "country", "target", "horizon"))
+
+bvar_scores_allyears <- scoreempQu(bvar_qu_allyears, cvg_rg = c(50,80),
+                              by = c("model", "country", "target", "horizon"))
+
+bvar_scores_avgcountry <- scoreempQu(bvar_qu_sameyearset, cvg_rg = c(50,80),
+                                by = c("model", "target", "horizon"))
+
+bvar_scores_cvgshort <- scoreempQu(bvar_qu_sameyearset, cvg_rg = c(50,80),
+                              by = c("model", "target"))
+
+
+
+
+
+######################################Saving#######################################################
 data.table::fwrite(scores, here("quantile_forecasts", "ci_scores.csv"))
 data.table::fwrite(scores_allyears, here("quantile_forecasts", "ci_scores_allyears.csv"))
-data.table::fwrite(all_crps, here("quantile_forecasts", "sample_scores.csv"))
 data.table::fwrite(scores_cvgshort, here("quantile_forecasts", "cvg_pooled.csv"))
 data.table::fwrite(scores_avgcountry, here("quantile_forecasts", "ci_scores_avgcnt.csv"))
+data.table::fwrite(all_crps, here("quantile_forecasts", "sample_scores.csv"))
 data.table::fwrite(pp_scores, here("quantile_forecasts", "pointfc_scores.csv"))
+data.table::fwrite(bvar_scores, here("quantile_forecasts", "bvar_ci_scores.csv"))
+data.table::fwrite(bvar_scores_allyears, here("quantile_forecasts", "bvar_ci_scores_allyears.csv"))
+data.table::fwrite(bvar_scores_cvgshort, here("quantile_forecasts", "bvar_cvg_pooled.csv"))
+data.table::fwrite(bvar_scores_avgcountry, here("quantile_forecasts", "bvar_ci_scores_avgcnt.csv"))
 
