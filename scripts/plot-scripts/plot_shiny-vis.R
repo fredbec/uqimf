c1c160c2f51cbafbb2d2c41fb4ee4d60bf1b649f
fredbec <f-becker@posteo.de>
2023-10-18 11:38:15 +0200

    change to single country output (patchwork wrap)

--- scripts/plot-scripts/plot_shiny-vis.R
+++ scripts/plot-scripts/plot_shiny-vis.R
@@ -2,6 +2,7 @@ library(data.table)
 library(here)
 library(ggplot2)
 library(MetBrewer)
+library(patchwork)
 devtools::load_all()
 
 .d <- `[`
@@ -68,57 +69,80 @@ labeldat_2023 <- linerange_dat |>
 
 colors <- met.brewer("Hokusai1", 7)
 names(colors) <- unique(qufcs$country)
-
-
 qus_list <- qu_lvls(cis)
+shinyplot <- function(realized_series,
+                      linerange_data,
+                      labels_currentyear,
+                      labels_nextyear,
+                      plot_country,
+                      colorscale){
+  ggplot() +
+    geom_line(
+      aes(x = target_year, y = true_value),
+      color = colorscale[plot_country],
+      data = realized_vals |> .d(country == plot_country),
+      lwd = 0.75) +
+    geom_point(
+      aes(x = target_year, y = true_value),
+      color = colorscale[plot_country],
+      data = realized_vals |> .d(country == plot_country),
+      size = 0.95) +
+    ggtitle(paste0("Actual Series, with forecast for year ", 2023)) +
+    ylab(plot_target_label()[trgt]) +
+    ylim(-0.5, 9.5) +
+    xlab("Target Year") +
+    ggtitle(plot_country_label()[plot_country]) +
+    scale_color_met_d("Hokusai1") +
+    theme_uqimf() %+replace%
+    theme(legend.position = "none",
+          plot.title = element_text(hjust = 0.5,
+                                    vjust = 3)) +
 
-ggplot() +
-  geom_line(
-    aes(x = target_year, y = true_value,
-        group = country, color = country),
-    data = realized_vals,
-    lwd = 0.75) +
-  geom_point(
-    aes(x = target_year, y = true_value,
-        group = country, color = country),
-    data = realized_vals,
-    size = 0.95) +
-  ggtitle(paste0("Actual Series, with forecast for year ", 2023)) +
-  ylab("True value") +
-  ylim(-0.5, 9.5) +
+    lapply(qus_list, function(qupr){
+      geom_linerange(
+        aes(x = target_year,
+            ymin = get(paste0("quantile", qupr[1])),
+            ymax = get(paste0("quantile", qupr[2]))),
+        color = colorscale[plot_country],
+        data = linerange_dat |> .d(country == plot_country),
+        lwd = 2.15, alpha = qupr[3], show.legend = TRUE)
+    }) +
 
-  facet_wrap(~country,
-             labeller = as_labeller(plot_country_label()),
-             nrow = 2, ncol = 4) +
-  xlab("Target Year") +
+    geom_point(
+      aes(x = target_year, y = prediction),
+      color = colorscale[plot_country],,
+      data = point_forecasts |> .d(country == plot_country),
+      size = 2.5
+    ) +
 
-  scale_color_met_d("Hokusai1") +
-  theme_uqimf() %+replace%
-  theme(legend.position = "none")+
+    geom_line(
+      aes(x = target_year, y = prediction),
+      color = colorscale[plot_country],
+      data = dashed_line |> .d(country == plot_country),
+      linetype = "dashed"
+    ) +
+    geom_label(data=labeldat_2022 |> .d(country == plot_country), aes(x=x, y=y, label=label),
+               color = colorscale[plot_country],
+               size=3.25 , angle=45, fontface="bold") +
+    geom_label(data=labeldat_2023 |> .d(country == plot_country), aes(x=x, y=y, label=label),
+               color = colorscale[plot_country],
+               size=3.25 , angle=45, fontface="bold")
 
-  lapply(qus_list, function(qupr){
-    geom_linerange(
-      aes(x = target_year,
-          ymin = get(paste0("quantile", qupr[1])),
-          ymax = get(paste0("quantile", qupr[2])),
-          color = country),
-      data = linerange_dat,
-      lwd = 2.15, alpha = qupr[3], show.legend = TRUE)
-  }) +
+}
 
-  geom_point(
-    aes(x = target_year, y = prediction, color = country),
-    data = point_forecasts,
-    size = 2.5
-  ) +
+plotlist <- lapply(as.list(unique(qufcs$country)),
+                   function(pltc) shinyplot(realized_vals, linerange_dat, labeldat_2022, labeldat_2023, pltc, colors))
 
-  geom_line(
-    aes(x = target_year, y = prediction, color = country),
-    data = dashed_line,
-    linetype = "dashed"
-  ) +
-  geom_label(data=labeldat_2022, aes(x=x, y=y, label=label, color = country),
-             size=3.25 , angle=45, fontface="bold") +
-  geom_label(data=labeldat_2023, aes(x=x, y=y, label=label, color = country),
-             size=3.25 , angle=45, fontface="bold")
+
+
+design <- "AAAAAAAA
+          #BBBBBB#
+          #CCCCCC#"
+
+
+wrap_elements(full = plotlist[[1]] + plotlist[[2]] + plotlist[[3]]) +
+wrap_elements(plotlist[[4]] + plotlist[[5]]) +
+  wrap_elements(plotlist[[6]]+ plotlist[[7]]) +
+  plot_layout(design = design)
+
 
