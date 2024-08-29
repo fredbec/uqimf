0f049115b28791324d6185a2824813a3a89df98d
fredbec <f-becker@posteo.de>
2024-08-29 13:42:32 +0200

    restructure III: remove old directional-symmetric code and files

--- scripts/make-forecasts.R
+++ scripts/make-forecasts.R
@@ -75,29 +75,5 @@ mainfcs_ew <- qufcs |>
 
 mainfcs <- rbind(mainfcs, mainfcs_ew)
 
-directional_expand <- qufcs |>
-  .d(error_method == "directional") |>
-  .d(quantile %in% c(0.1+fperror(type = 0.1), 0.25, 0.75, 0.9)) |>
-  .d(, .(country, target, target_year, horizon, imf_pp, true_value, error, quantile, error_prediction, source, method)) |>
-  .d(, quantile := paste0("q", quantile)) |>
-  dcast(country + target + target_year + horizon + error + imf_pp + true_value + source + method ~ quantile,
-         value.var = "error_prediction") |>
-  .d(,length_ci0.9 := q0.9 - q0.1) |>
-  .d(,length_ci0.5 := q0.75 - q0.25) |>
-  .d(, q0.1 := -length_ci0.9/2) |>
-  .d(, q0.9 := length_ci0.9/2) |>
-  .d(, q0.25 := -length_ci0.5/2) |>
-  .d(, q0.75 := length_ci0.5/2) |>
-  .d(, length_ci0.9 := NULL) |>
-  .d(, length_ci0.5 := NULL) |>
-  melt(measure.vars = c(paste0("q", c(0.1, 0.25, 0.75, 0.9))),
-       value.name = "error_prediction",
-       variable.name = "quantile") |>
-  .d(, quantile := as.numeric(substring(quantile, 2))) |>
-  .d(, error_method := "symmetric") |>
-  .d(, prediction := imf_pp + error_prediction)
-
-
 data.table::fwrite(qufcs, here("quantile_forecasts", "quantile_forecasts.csv"))
-data.table::fwrite(directional_expand, here("quantile_forecasts", "qufcs_directionalsymmetric.csv"))
 data.table::fwrite(mainfcs, here("quantile_forecasts", "quantile_forecasts_pava.csv"))
