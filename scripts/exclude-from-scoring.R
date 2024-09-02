020d8ff5ee6ff703d55b73f62032e453dc4945b1
fredbec <f-becker@posteo.de>
2024-09-02 11:22:15 +0200

    restructure IV, part III: remove everything not relevant for final analysis from data processing (exclude-from-scoring)

--- scripts/exclude-from-scoring.R
+++ scripts/exclude-from-scoring.R
@@ -37,19 +37,30 @@ data.table::fwrite(bvar_qus, here("benchmarks", "toscore_bvar_direct_quantile_fo
 data.table::fwrite(fcdat, here("data", "toscore_point_forecasts.csv"))
 
 
-###again for scoring the bvar specifications, only for holdout set
 ####################################read in quantile forecasts########################
+qufcs <- data.table::fread(here("quantile_forecasts", "quantile_forecasts.csv"))
 qufcs_ho <- data.table::fread(here("quantile_forecasts", "quantile_forecasts_ho.csv"))
+qufcs_directional <- data.table::fread(here("quantile_forecasts", "quantile_forecasts_directional.csv"))
+bvar_qus <- data.table::fread(here("benchmarks", "bvar_direct_quantile_forecasts.csv"))
 bvar_qus_ho <- data.table::fread(here("benchmarks", "bvar_direct_quantile_forecasts_ho.csv"))
 
 for(k in 1:length(instances_to_exclude_bvarspecs)){
 
   inst <- instances_to_exclude_bvarspecs[[k]]
+  qufcs <- exclude_rows(qufcs, inst)
   qufcs_ho <- exclude_rows(qufcs_ho, inst)
+  qufcs_directional <- exclude_rows(qufcs_directional, inst)
+  bvar_qus <- exclude_rows(bvar_qus, inst)
   bvar_qus_ho <- exclude_rows(bvar_qus_ho, inst)
+  fcdat <- exclude_rows(fcdat, inst)
 }
 
 
 data.table::fwrite(qufcs, here("quantile_forecasts", "toscore_bvarspecs_quantile_forecasts.csv"))
-data.table::fwrite(bvar_qus_ho, here("benchmarks", "toscore_bvarspecs_bvar_direct_quantile_forecasts_ho.csv"))
+data.table::fwrite(qufcs_directional, here("quantile_forecasts", "toscore_bvarspecs_quantile_forecasts_directional.csv"))
+data.table::fwrite(qufcs_ho, here("quantile_forecasts", "toscore_bvarspecs_quantile_forecasts_ho.csv"))
+data.table::fwrite(bvar_qus, here("benchmarks", "toscore_bvarspecs_bvar_direct_quantile_forecasts.csv"))
+data.table::fwrite(bvar_qus, here("benchmarks", "toscore_bvarspecs_bvar_direct_quantile_forecasts_ho.csv"))
+data.table::fwrite(fcdat, here("data", "toscore_bvarspecs_point_forecasts.csv"))
+
 
