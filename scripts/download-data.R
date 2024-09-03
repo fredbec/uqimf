57318e8908cc4be601c614b746a79595d7d1fce2
fredbec <f-becker@posteo.de>
2024-09-03 18:21:40 +0200

    restructure VI, small things: consolidate raw WEO data to parquet file

--- scripts/download-data.R
+++ scripts/download-data.R
@@ -1,26 +1,19 @@
 library(here)
+library(arrow)
 
 DT <- `[`
 
+
+#imfpp is an R package that can be downloaded from GitHub with the following command:
+#devtools::install_github("https://github.com/fredbec/imfpp")
+
+#then downdload IMF data
 imfpp::download.process.weo(includeGeoData = FALSE)
+
+#this alos downloads two datafiles, WB_LFP, WB_GDPpC, these can be deleted
 
 weodat <- data.table::fread(here("WEOforecasts_tidy.csv"))
 
-N <- nrow(weodat)
-
-
-#split data into 10 subdatasets (for uploading to GitHub)
-weodat |>
-  DT(, splitinds := rep(1:10, each = N/10)) |>
-  split(by = "splitinds") |>
-  lapply(function(dat){
-
-    splitind <- unique(dat$splitinds)
-
-    dat |>
-      DT(, splitinds := NULL) |>
-      data.table::fwrite(here("data", paste0("WEOforecasts", splitind, ".csv")))
-    }
-) |>
-  unlist() #just for nicer printed output
-
+#save as parquet to limit file size for GitHub
+write_parquet(weodat, here("data", "WEOforecasts_prefilter.parquet"))
+file.remove(here("WEOforecasts_tidy.csv"))
--- scripts/process-weo.R
+++ scripts/process-weo.R
@@ -1,12 +1,13 @@
 library(here)
 library(data.table)
+library(arrow)
 
 
 .d <- `[`
 
 #short script to process WEO data for this project
 
-weodat <- data.table::fread(here("WEOforecasts_tidy.csv")) |>
+weodat <- read_parquet(here("data", "WEOforecasts_prefilter.parquet")) |>
   .d(g7 == 1) |> #only keep g7 countries
   .d(, .(ISOAlpha_3Code, target, target_year, forecast_season, horizon,
          forecast_year, prediction, tv_0.5, tv_1, tv_1.5, tv_2)) |>
